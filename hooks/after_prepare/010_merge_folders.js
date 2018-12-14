94244e6cadecd3effec6935b68dde529cb86c190
Joel Liang <liangyuzhou@elastos.org>
2018-12-14 16:22:59 +0800

    Added hook scripts and patches for ios build

--- hooks/after_prepare/010_merge_folders.js
+++ hooks/after_prepare/010_merge_folders.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --use_strict
 
 //
 // This hook copies various resource files/folders
---
+++ hooks/after_prepare/030_modify_xcode_project.js
@@ -0,0 +1,152 @@
+#!/usr/bin/env node
+"use strict";
+
+let xcode = require('xcode'),
+    fs = require('fs'),
+    path = require('path'),
+    runtimeProjPath = 'platforms/ios/Runtime.xcodeproj/project.pbxproj',
+    runtimeProj = xcode.project(runtimeProjPath),
+    cordovaProjPath = 'platforms/ios/CordovaLib/CordovaLib.xcodeproj/project.pbxproj',
+    cordovaProj = xcode.project(cordovaProjPath);
+
+runtimeProj.parse(function (err) {
+    //
+    // Embed frameworks and binaries
+    //
+    let embed = true;
+    let existsEmbedFrameworks = runtimeProj.buildPhaseObject('PBXCopyFilesBuildPhase', 'Embed Frameworks');
+    if (!existsEmbedFrameworks && embed) {
+        // "Embed Frameworks" Build Phase (Embedded Binaries) does not exist, creating it.
+        runtimeProj.addBuildPhase([], 'PBXCopyFilesBuildPhase', 'Embed Frameworks', null, 'frameworks');
+    }
+
+    let options = { customFramework: true, embed: embed };
+    runtimeProj.addFramework('RealmSwift.framework', options);
+    runtimeProj.addFramework('Realm.framework', options);
+    runtimeProj.addFramework('libz.tbd');
+
+
+    //
+    // Add build settings
+    //
+    runtimeProj.addToBuildSettings("SWIFT_VERSION", "4.2");
+    runtimeProj.addToBuildSettings("PRODUCT_BUNDLE_IDENTIFIER", "org.elastos.trinity.runtime");
+
+
+    //
+    // Add and remove source files in the Classes group
+    //
+    let classesGroupKey = runtimeProj.findPBXGroupKeyAndType({ name: 'Classes' }, 'PBXGroup');
+
+    runtimeProj.removeSourceFile("MainViewController.h",      {}, classesGroupKey);
+    runtimeProj.removeSourceFile("MainViewController.m",      {}, classesGroupKey);
+
+    runtimeProj.addSourceFile("AppBasePlugin.swift",          {}, classesGroupKey);
+    runtimeProj.addSourceFile("AppInfo.swift",                {}, classesGroupKey);
+    runtimeProj.addSourceFile("AppInstaller.swift",           {}, classesGroupKey);
+    runtimeProj.addSourceFile("AppManager.swift",             {}, classesGroupKey);
+    runtimeProj.addSourceFile("AppViewController.swift",      {}, classesGroupKey);
+    runtimeProj.addSourceFile("AppWhitelist.swift",           {}, classesGroupKey);
+    runtimeProj.addSourceFile("AppXmlParser.swift",           {}, classesGroupKey);
+    runtimeProj.addSourceFile("AuthorityPlugin.swift",        {}, classesGroupKey);
+    runtimeProj.addSourceFile("LauncherViewController.swift", {}, classesGroupKey);
+    runtimeProj.addSourceFile("MainViewController.swift",     {}, classesGroupKey);
+    runtimeProj.addSourceFile("ManagerDBAdapter.swift",       {}, classesGroupKey);
+    runtimeProj.addSourceFile("NullPlugin.swift",             {}, classesGroupKey);
+    runtimeProj.addSourceFile("Runtime-Bridging-Header.h",    {}, classesGroupKey);
+    runtimeProj.addSourceFile("TrinityPlugin.h",              {}, classesGroupKey);
+    runtimeProj.addSourceFile("TrinityPlugin.m",              {}, classesGroupKey);
+    runtimeProj.addSourceFile("TrinityViewController.swift",  {}, classesGroupKey);
+    runtimeProj.addSourceFile("Utility.swift",                {}, classesGroupKey);
+    runtimeProj.addSourceFile("WhitelistFilter.swift",        {}, classesGroupKey);
+
+
+    //
+    // Add header and source files for "SSZipArchive"
+    //
+    let sourceDirectory = "platforms/ios/SSZipArchive";
+
+    function fromDir(startPath,filter,callback){
+        if (!fs.existsSync(startPath)){
+            console.log("no dir ",startPath);
+            return;
+        }
+
+        let files=fs.readdirSync(startPath);
+        for(let i=0;i<files.length;i++){
+            let filename = files[i];
+            let filepath = path.join(startPath,filename);
+            let stat = fs.lstatSync(filepath);
+            if (stat.isDirectory()){
+                callback(startPath, filename);
+                fromDir(filepath,filter,callback); //recurse
+            }
+            else if (filter.test(filepath)) callback(startPath, filename);
+        };
+    };
+
+    fromDir(sourceDirectory,/(\.h$|\.m$|\.c$)/,function(startPath,filename){
+        let filepath = path.join(startPath,filename);
+        let relativePath = startPath.replace(/platforms\/ios\//g, '');
+
+        let parentGroupName = path.basename(relativePath);
+        let parentGroupKey = runtimeProj.findPBXGroupKeyAndType({ name: parentGroupName }, 'PBXGroup');
+        if (!parentGroupKey) {
+            let parentGroupPath = '"' +parentGroupName + '"';
+            let newGroup = runtimeProj.addPbxGroup([], parentGroupName, parentGroupPath, 'SOURCE_ROOT');
+            let customTemplate = runtimeProj.findPBXGroupKeyAndType({ name: 'CustomTemplate' }, 'PBXGroup');
+            runtimeProj.addToPbxGroup(newGroup.uuid, customTemplate, 'PBXGroup');
+            parentGroupKey = newGroup.uuid;
+        }
+
+        let stat = fs.lstatSync(filepath);
+        if (stat.isDirectory()){
+            let groupName = filename;
+            let groupPath = '"' + path.join(relativePath, groupName) + '"';
+            let newGroup = runtimeProj.addPbxGroup([], groupName, groupPath, 'SOURCE_ROOT');
+            runtimeProj.addToPbxGroup(newGroup.uuid, parentGroupKey, 'PBXGroup');
+            return;
+        } else {
+            let groupName = path.basename(startPath);
+            let groupKey = runtimeProj.findPBXGroupKeyAndType({ name: groupName }, 'PBXGroup');
+            if (groupKey) {
+                runtimeProj.addSourceFile(filename, {}, groupKey);
+            }
+        }
+    });
+
+
+    //
+    // Add a "Run Script" build phase
+    //
+    options = {
+        shellPath: '/bin/sh',
+        shellScript: 'bash "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/Realm.framework/strip-frameworks.sh"'
+    };
+    runtimeProj.addBuildPhase([], 'PBXShellScriptBuildPhase', 'Run Script', runtimeProj.getFirstTarget().uuid, options).buildPhase;
+
+
+    //
+    // Write back the new XCode project
+    //
+    fs.writeFileSync(runtimeProjPath, runtimeProj.writeSync());
+});
+
+cordovaProj.parse(function (err) {
+    //
+    // Make the "CDVIntentAndNavigationFilter.h" file public
+    //
+    let uuid;
+    for (uuid in cordovaProj.pbxBuildFileSection()) {
+        if (cordovaProj.pbxBuildFileSection()[uuid].fileRef_comment == 'CDVIntentAndNavigationFilter.h') {
+            let file = cordovaProj.pbxBuildFileSection()[uuid];
+            file.settings =  { ATTRIBUTES: [ 'Public' ] };
+        }
+    }
+
+
+    //
+    // Write back the new XCode project
+    //
+    fs.writeFileSync(cordovaProjPath, cordovaProj.writeSync());
+});
