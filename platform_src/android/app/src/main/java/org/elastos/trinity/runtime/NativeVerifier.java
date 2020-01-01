513930bc0d030e853aa167db8bcc990ef6ceeab7
kuit <kuit@msn.com>
2020-01-01 16:57:53 +0800

    delete cpp files

--- platform_src/android/app/src/main/java/org/elastos/trinity/runtime/NativeVerifier.java
+++
@@ -1,9 +0,0 @@
-package org.elastos.trinity.runtime;
-
-public class NativeVerifier {
-    public native static boolean verify(String public_key, String payload, String signed_payload);
-
-    static {
-        System.loadLibrary("verifier");
-    }
-}
